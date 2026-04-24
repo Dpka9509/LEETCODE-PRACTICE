e45c9c8a9fb3e400e5a0ef879299c54f955b87e3
Deepika Srivastava <dpka9509@gmain.com>
2026-04-24 22:42:02 +0530

    [Deepika] Added:day3 problem 3

---
+++ removen.java
@@ -0,0 +1,56 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class removen {
+    public static void main(String[] args) {
+        // Create list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int n = 2; // remove 2nd node from end
+
+        int sz = 0;
+        ListNode temp = head;
+
+        // Calculate size
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+
+        // If removing head
+        if (sz == n) {
+            head = head.next;
+        } else {
+            int i = 1;
+            int idx = sz - n;
+
+            ListNode prev = head;
+
+            while (i < idx) {
+                prev = prev.next;
+                i++;
+            }
+
+            prev.next = prev.next.next;
+        }
+
+        // Print updated list
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+}
